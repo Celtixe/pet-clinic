d52f8a4abdc9c35a6de02cf3cefcbef8cc18db26
Mikolaj Miller <mikolaj.miller@kcl.ac.uk>
2019-08-18 13:44:32 +0100

    List all the owners on the owners index page closes #16

--- pet-clinic-web/src/main/java/com/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/petclinic/controllers/OwnerController.java
@@ -1,13 +1,27 @@
 package com.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.petclinic.model.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	@Autowired
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping("owners")
-	public String listOwners() {
+	public String listOwners(Model m) {
+		m.addAttribute("owners", ownerService.findAll() );
 		return "owners/index";
 	}
 }
