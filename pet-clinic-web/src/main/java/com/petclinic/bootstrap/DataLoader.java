fb49a4c3e99f7d6de16e8a3edf74f62b04bd9fc8
Mikolaj Miller <mikolaj.miller@kcl.ac.uk>
2019-08-20 18:53:18 +0100

    Add pet types in with startup closes #29

--- pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/petclinic/bootstrap/DataLoader.java
@@ -1,12 +1,13 @@
 package com.petclinic.bootstrap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.petclinic.model.Owner;
+import com.petclinic.model.PetType;
 import com.petclinic.model.Vet;
 import com.petclinic.model.services.OwnerService;
+import com.petclinic.model.services.PetTypeService;
 import com.petclinic.model.services.VetService;
 
 
@@ -15,19 +16,30 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 	
-	@Autowired
-	DataLoader(OwnerService ownerService, VetService vetService) {
-		super();
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
+
+
 
 
 
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("Dog");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Fiona");
 		owner1.setLastName("Gleanna");
